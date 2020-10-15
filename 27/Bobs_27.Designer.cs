9fbdd1bb09c54e76edfc576be8413cdd347803ec
Chunkyy <35497034+Chunkyy@users.noreply.github.com>
2020-10-15 10:25:40 +0100

    Add files via upload

---
+++ 27/Bobs_27.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace _27
+{
+    partial class Bobs_27
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.oneBtn = new System.Windows.Forms.Button();
+            this.twoBtn = new System.Windows.Forms.Button();
+            this.threeBtn = new System.Windows.Forms.Button();
+            this.boardNumber = new System.Windows.Forms.Label();
+            this.dblValue = new System.Windows.Forms.Label();
+            this.score = new System.Windows.Forms.Label();
+            this.scoreValue = new System.Windows.Forms.Label();
+            this.missBtn = new System.Windows.Forms.Button();
+            this.ngBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // oneBtn
+            // 
+            this.oneBtn.Location = new System.Drawing.Point(7, 104);
+            this.oneBtn.Name = "oneBtn";
+            this.oneBtn.Size = new System.Drawing.Size(131, 61);
+            this.oneBtn.TabIndex = 0;
+            this.oneBtn.Text = "1";
+            this.oneBtn.UseVisualStyleBackColor = true;
+            this.oneBtn.Click += new System.EventHandler(this.oneBtn_Click);
+            // 
+            // twoBtn
+            // 
+            this.twoBtn.Location = new System.Drawing.Point(144, 104);
+            this.twoBtn.Name = "twoBtn";
+            this.twoBtn.Size = new System.Drawing.Size(131, 61);
+            this.twoBtn.TabIndex = 1;
+            this.twoBtn.Text = "2";
+            this.twoBtn.UseVisualStyleBackColor = true;
+            this.twoBtn.Click += new System.EventHandler(this.twoBtn_Click);
+            // 
+            // threeBtn
+            // 
+            this.threeBtn.Location = new System.Drawing.Point(281, 104);
+            this.threeBtn.Name = "threeBtn";
+            this.threeBtn.Size = new System.Drawing.Size(131, 61);
+            this.threeBtn.TabIndex = 2;
+            this.threeBtn.Text = "3";
+            this.threeBtn.UseVisualStyleBackColor = true;
+            this.threeBtn.Click += new System.EventHandler(this.threeBtn_Click);
+            // 
+            // boardNumber
+            // 
+            this.boardNumber.AutoSize = true;
+            this.boardNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boardNumber.Location = new System.Drawing.Point(35, 9);
+            this.boardNumber.Name = "boardNumber";
+            this.boardNumber.Size = new System.Drawing.Size(81, 26);
+            this.boardNumber.TabIndex = 3;
+            this.boardNumber.Text = "Double";
+            // 
+            // dblValue
+            // 
+            this.dblValue.AutoSize = true;
+            this.dblValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dblValue.Location = new System.Drawing.Point(31, 35);
+            this.dblValue.Name = "dblValue";
+            this.dblValue.Size = new System.Drawing.Size(46, 51);
+            this.dblValue.TabIndex = 4;
+            this.dblValue.Text = "1";
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score.Location = new System.Drawing.Point(307, 9);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(69, 26);
+            this.score.TabIndex = 5;
+            this.score.Text = "Score";
+            // 
+            // scoreValue
+            // 
+            this.scoreValue.AutoSize = true;
+            this.scoreValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreValue.Location = new System.Drawing.Point(294, 35);
+            this.scoreValue.Name = "scoreValue";
+            this.scoreValue.Size = new System.Drawing.Size(70, 51);
+            this.scoreValue.TabIndex = 6;
+            this.scoreValue.Text = "27";
+            // 
+            // missBtn
+            // 
+            this.missBtn.Location = new System.Drawing.Point(144, 35);
+            this.missBtn.Name = "missBtn";
+            this.missBtn.Size = new System.Drawing.Size(131, 61);
+            this.missBtn.TabIndex = 7;
+            this.missBtn.Text = "Miss";
+            this.missBtn.UseVisualStyleBackColor = true;
+            this.missBtn.Click += new System.EventHandler(this.missBtn_Click);
+            // 
+            // ngBtn
+            // 
+            this.ngBtn.Location = new System.Drawing.Point(175, 6);
+            this.ngBtn.Name = "ngBtn";
+            this.ngBtn.Size = new System.Drawing.Size(75, 23);
+            this.ngBtn.TabIndex = 8;
+            this.ngBtn.Text = "New Game";
+            this.ngBtn.UseVisualStyleBackColor = true;
+            this.ngBtn.Click += new System.EventHandler(this.NgBtn_Click);
+            // 
+            // Bobs_27
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 173);
+            this.Controls.Add(this.ngBtn);
+            this.Controls.Add(this.missBtn);
+            this.Controls.Add(this.scoreValue);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.dblValue);
+            this.Controls.Add(this.boardNumber);
+            this.Controls.Add(this.threeBtn);
+            this.Controls.Add(this.twoBtn);
+            this.Controls.Add(this.oneBtn);
+            this.Name = "Bobs_27";
+            this.Text = "27";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button oneBtn;
+        private System.Windows.Forms.Button twoBtn;
+        private System.Windows.Forms.Button threeBtn;
+        private System.Windows.Forms.Label boardNumber;
+        private System.Windows.Forms.Label dblValue;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Label scoreValue;
+        private System.Windows.Forms.Button missBtn;
+        private System.Windows.Forms.Button ngBtn;
+    }
+}
+
---
+++ 27/Bobs_27.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _27
+{
+    public partial class Bobs_27 : Form
+    {
+        int d = 1;
+        int sv = 27;
+        int calc;
+        int dartValue;
+
+        public Bobs_27()
+        {
+            InitializeComponent();
+        }
+
+        private void oneBtn_Click(object sender, EventArgs e)
+        {
+            dartValue = 1;
+            calculate();
+        }
+
+        private void twoBtn_Click(object sender, EventArgs e)
+        {
+            dartValue = 2;
+            calculate();
+        }
+
+        private void threeBtn_Click(object sender, EventArgs e)
+        {
+            dartValue = 3;
+            calculate();
+        }
+
+        private void missBtn_Click(object sender, EventArgs e)
+        {
+            calc = sv - (d * 2);
+            sv = calc;
+            scoreValue.Text = sv.ToString();
+
+            updateScore();
+        }
+
+        public void incrementDouble()
+        {
+            d++;
+
+            if (d == 21)
+            {
+                d = 25;
+                dblValue.Text = "Bull";
+                boardNumber.Visible = false;
+            }
+            else if (d == 26)
+            {
+                MessageBox.Show("You've won! Your score is " + sv.ToString() + "!");
+                newGame();
+            }
+            else
+            {
+                dblValue.Text = d.ToString();
+            }
+        }
+
+        public void newGame()
+        {
+            scoreValue.ForeColor = Color.Black;
+            d = 1;
+            sv = 27;
+            dblValue.Text = d.ToString();
+            scoreValue.Text = sv.ToString();
+            boardNumber.Visible = true;
+        }
+
+        public void updateScore()
+        {
+            if (sv <= 0)
+            {
+                scoreValue.ForeColor = Color.Red;
+                MessageBox.Show("Bust, you finished on " + d.ToString());
+                newGame();
+            }
+            else if (sv - (d * 2) <= 0)
+            {
+                scoreValue.ForeColor = Color.Orange;
+                incrementDouble();
+            }
+            else if (sv - (d * 2) > 0)
+            {
+                scoreValue.ForeColor = Color.Black;
+                incrementDouble();
+            }
+        }
+
+        public void calculate()
+        {            
+            calc = (d * 2) * dartValue + sv;
+            sv = calc;
+            scoreValue.Text = sv.ToString();
+            
+            updateScore();
+        }
+
+        private void NgBtn_Click(object sender, EventArgs e)
+        {
+            newGame();
+        }
+    }
+}
